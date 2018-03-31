a2a7347b4a72969bfd0483deb945b2d4ef832fc4
최철환 <hwanyhee@gmail.com>
2018-03-31 16:13:44 +0900

    jquery 주소검색
    1. index.html에서 로딩하는 라이브러리를 폼으로 이동
    2. jquery 바인딩시, 값을 가져오는 부분 (수정필요)

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {MemberVO} from '../../domain/member.vo';
 import {MatSnackBar, MatSnackBarConfig} from '@angular/material';
 import {UserService} from '../../user.service';
 import {AuthGuardService} from '../auth-guard.service';
 import {Router} from '@angular/router';
+
+declare var $: any;
 
 @Component({
   selector: 'app-register',
@@ -15,9 +17,10 @@ export class RegisterComponent implements OnInit {
 
   public form: FormGroup;
   member: MemberVO;
+  url = "//d1p7wdleee1q2z.cloudfront.net/post/search.min.js";
 
   constructor(private snackBar: MatSnackBar, private userService: UserService, private authGuard: AuthGuardService
-    , private router: Router, private fb: FormBuilder) {
+    , private router: Router, private fb: FormBuilder, private el: ElementRef) {
 
     this.form = this.fb.group({
       nickname: new FormControl(null, [Validators.required, Validators.minLength(4)]),
@@ -31,6 +34,28 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.member = JSON.parse(localStorage.getItem('member'));
+
+    // index.html에서 로딩되는 라이브러리를 회원가입 페이지에서만 로딩되도록 변경
+    // $("#postcode").postcodifyPopUp();
+    new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = this.url;
+      script.type = 'text/javascript';
+      script.async = true;
+      this.el.nativeElement.appendChild(script);
+      resolve(true);
+    }).then(() => {
+      setTimeout(() => {
+        $(function() { $("#postcode").postcodifyPopUp(); });
+        // jquery로 폼이 바뀌면 angular로 인식이 안되기 때문에 이벤트를 받아서 angular폼을 세팅
+        $('.postcodify_popup_layer').on('click', () => {
+          console.log('click');
+          this.form.controls['postcode'].setValue($('.postcodify_postcode5').val());
+          this.form.controls['address'].setValue($('.postcodify_address').val());
+        });
+      }, 2000);
+    });
+
   }
 
 
